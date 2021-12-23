6e97bb5b919e5d50d89b02fd5bd0f1dbf56c8c47
jedinym <jedinym@gmail.com>
2021-12-23 22:27:40 +0100

    Implemented spotify and stdin/stdout

--- muse/objects.py
+++ muse/objects.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any
 
+
 class Track:
     def __init__(self, name: str, album: str, artists: list[str]):
         self.name = name
@@ -7,21 +8,28 @@ class Track:
         self.artists = artists
 
     @staticmethod
-    def make_track(info: Dict[Any, Any]) -> 'Track':
-        track_name = info['track']['name']
-        track_album = info['track']['album']['name']
-        artists = info['track']['artists']
+    def make_track(info: Dict[Any, Any]) -> "Track":
+        track_name = info["track"]["name"]
+        track_album = info["track"]["album"]["name"]
+        artists = info["track"]["artists"]
         track_artists = []
         for artist in artists:
-            track_artists.append(artist['name'])
-        
+            track_artists.append(artist["name"])
+
         return Track(track_name, track_album, track_artists)
 
     def __str__(self) -> str:
-        return f'{self.artists} - {self.album} - {self.name}'
+        return f"{self.artists} - {self.album} - {self.name}"
 
     def csv(self, sep: str) -> str:
-        return f'{self.artists}{sep}{self.album}{sep}{self.name}'
+        return f"{self.artists}{sep}{self.album}{sep}{self.name}"
+
+    def dictify(self) -> Dict[str, str | list[str]]:
+        track: Dict[str, str | list[str]] = dict()
+        track["name"] = self.name
+        track["album"] = self.album
+        track["artists"] = self.artists
+        return track
 
 
 class Album:
@@ -30,17 +38,22 @@ class Album:
         self.artists = _artists
 
     @staticmethod
-    def make_album(info: Dict[Any, Any]) -> 'Album':
-        album_name = info['album']['name']
-        artists = info['album']['artists']
+    def make_album(info: Dict[Any, Any]) -> "Album":
+        album_name = info["album"]["name"]
+        artists = info["album"]["artists"]
         album_artists = []
         for artist in artists:
-            album_artists.append(artist['name'])
+            album_artists.append(artist["name"])
         return Album(album_name, album_artists)
 
     def __str__(self) -> str:
-        return f'{self.artists} - {self.name}'
+        return f"{self.artists} - {self.name}"
 
     def csv(self, sep: str) -> str:
-        return f'{self.artists}{sep}{self.name}'
+        return f"{self.artists}{sep}{self.name}"
 
+    def dictify(self) -> Dict[str, str | list[str]]:
+        album: Dict[str, str | list[str]] = dict()
+        album["name"] = self.name
+        album["artists"] = self.artists
+        return album
---
+++ muse/writers.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Optional
+from abc import ABC, abstractmethod
+import sys
+from sys import stdout, stderr
+import json
+
+from spotipy import Spotify
+
+from objects import Album, Track
+
+
+class Writer(ABC):
+    @abstractmethod
+    def add_saved_tracks(self, tracks: list[Track]) -> None:
+        raise NotImplemented
+
+    @abstractmethod
+    def add_saved_albums(self, albums: list[Album]) -> None:
+        raise NotImplemented
+
+    @staticmethod
+    def get_writer(dest: str) -> "Writer":
+        if dest == "spotify":
+            sptf = spotipy.Spotify(
+                auth_manager=SpotifyOAuth(scope=scope),
+                client_credentials_manager=SpotifyClientCredentials(),
+            )
+            return SpotifyWriter(sptf)
+        elif dest == "stdout":
+            return FileWriter(stdout)
+
+        print(f"Destination {dest} does not exist", file=stderr)
+        exit(1)
+
+    def write_objects(self, obj_type: str, objects: list[Track] | list[Album]) -> None:
+        if obj_type == "tracks":
+            self.add_saved_tracks(objects)
+        elif obj_type == "albums":
+            self.add_saved_albums(objects)
+        else:
+            raise NotImplemented
+
+
+class SpotifyWriter(Writer):
+    def __init__(self, _sptf: Spotify):
+        self.sptf = _sptf
+
+    def get_track_id(self, track: Track) -> Optional[str]:
+        results = self.sptf.search(
+            f'artist:"{track.artists[0]}"+track:"{track.name}"', type="track"
+        )
+        tracks = results["tracks"]["items"]
+        if not tracks:
+            return None
+        return tracks[0]["id"]
+
+    def get_album_id(self, album: Album) -> Optional[str]:
+        results = self.sptf.search(
+            f'artist:"{album.artists[0]}"+album:"{album.name}"', type="album"
+        )
+        albums = results["albums"]["items"]
+        if not albums:
+            return None
+        return albums[0]["id"]
+
+    def add_saved_tracks(self, tracks: list[Track]) -> None:
+        track_ids = []
+        for track in tracks:
+            id = self.get_track_id(track)
+            if id is None:
+                print(f"Track {str(track)} not found!", file=sys.stderr)
+            else:
+                track_ids.append(id)
+        self.sptf.current_user_saved_tracks_add(track_ids)
+
+    def add_saved_albums(self, albums: list[Album]) -> None:
+        album_ids = []
+        for album in albums:
+            id = self.get_album_id(album)
+            if id is None:
+                print(f"Album {str(album)} not found!", file=sys.stderr)
+            else:
+                album_ids.append(id)
+        # self.sptf.current_user_saved_albums_add(album_ids)
+
+
+class FileWriter(Writer):
+    def __init__(self, _file):
+        self.file = _file
+
+    def add_saved_tracks(self, tracks: list[Track]) -> None:
+        dictified = map(lambda x: x.dictify(), tracks)
+        track_dict = {"tracks": list(dictified)}
+        self.file.write(json.dumps(track_dict, indent=4))
+
+    def add_saved_albums(self, albums: list[Album]) -> None:
+        dictified = map(lambda x: x.dictify(), albums)
+        album_dict = {"albums": list(dictified)}
+        self.file.write(json.dumps(album_dict, indent=4))
